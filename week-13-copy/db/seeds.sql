60cfe3c21344a2f6ffbb6bda8f1b41bc94762ddd
tehpunisher456 <55720542+tehpunisher456@users.noreply.github.com>
2020-02-24 20:10:19 -0800

    added separate repo

--- week-13-copy/db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO burgers (burger_name,devoured) values('Basic Burger', FALSE);
-INSERT INTO burgers (burger_name,devoured) values('Not Basic Burger', FALSE);
-INSERT INTO burgers (burger_name,devoured) values('Interesting Burger', FALSE);
-INSERT INTO burgers (burger_name,devoured) values('Ultimate Burger', FALSE);
